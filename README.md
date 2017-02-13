f9aae25bea42c705a222301c659e5c3f508fadbc
Hiep Sy <hiepsy0704@gmail.com>
2017-02-13 11:37:32 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# View http://syinc.net/ to see me!
+
+## Created by Sy Dinh - Web Developer
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('.countdown').countdown({
+        date: "February 31, 2017 23:59:59"
+    });
+});
