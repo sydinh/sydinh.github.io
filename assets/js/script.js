13aec44f14c456ed2df3a1574ef319b39894bc9e
sydinh <sydinh.dev@gmail.com>
2017-07-23 09:35:53 +0700

    update some file :smile:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 $(function() {
   $('.countdown').countdown({
-    date: "Aug 31, 2017 23:59:59"
+    date: 'Aug 31, 2017 23:59:59'
   });
 
-  $(".skype-link").hover(function () {
-      $(".skype").toggleClass("skype-visible");
+  $('.skype-link').hover(function () {
+      $('.skype').toggleClass('skype-visible');
   });
 });
 
 function openNav() {
-  $(".trigger-content").addClass("trigger-width");
-  document.getElementById("trigger").style.display = "none";
+  $('.trigger-content').addClass('trigger-width');
+  document.getElementById('trigger').style.display = 'none';
 }
 
 function closeNav() {
-  $(".trigger-content").removeClass("trigger-width");
-  document.getElementById("trigger").style.display = "block";
+  $('.trigger-content').removeClass('trigger-width');
+  document.getElementById('trigger').style.display = 'block';
 }
