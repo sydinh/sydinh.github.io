b923062b4de7d53d0c3d937f479f435c768322a0
Sy Dinh <sydinh.dev@gmail.com>
2018-02-22 11:10:51 +0700

    Update main.js
    #UpdateMainJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-$(function(){$(".countdown").countdown({date:"FEB 28, 2018 23:59:59"}),$(".skype-link").hover(function(){$(".skype").toggleClass("skype-visible")})});
+$(function(){$(".countdown").countdown({date:"MAR 31, 2018 23:59:59"}),$(".skype-link").hover(function(){$(".skype").toggleClass("skype-visible")})});
