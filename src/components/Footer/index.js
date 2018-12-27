e52e7a1af804cd4e4740bc3c817e29e14e34c131
sydinh <sydinh.dev@gmail.com>
2018-12-27 14:15:06 +0700

    Replace source code

--- src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer role="contentinfo" className="footer">
-    Copyright &copy; 2019
-  </footer>
-);
-
-export default Footer;
--- src/constants/routes.js
+++
@@ -1,4 +0,0 @@
-export const HOME = '/';
-export const ABOUT = '/about';
-export const WORK = '/work';
-export const CONTACT = '/contact';
