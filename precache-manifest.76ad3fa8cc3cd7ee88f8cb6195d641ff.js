40b0e60ad077cb5cf388bded570b53addc9c9c15
sydinh <sydinh.dev@gmail.com>
2018-12-27 14:06:59 +0700

    Updates

---
+++ precache-manifest.76ad3fa8cc3cd7ee88f8cb6195d641ff.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4b332a8100def7c3890",
+    "url": "/static/js/runtime~main.d4b332a8.js"
+  },
+  {
+    "revision": "531c16b3ddd357a45fc2",
+    "url": "/static/js/0.531c16b3.chunk.js"
+  },
+  {
+    "revision": "45ea5345c4aa76cc51a2ef562577115a",
+    "url": "/static/media/linkedin.45ea5345.svg"
+  },
+  {
+    "revision": "77be88a643115a1f6c21",
+    "url": "/static/js/main.77be88a6.chunk.js"
+  },
+  {
+    "revision": "181e48ff75a0a795a8bc",
+    "url": "/static/js/3.181e48ff.chunk.js"
+  },
+  {
+    "revision": "2595b1edff25efb0fb4f",
+    "url": "/static/js/4.2595b1ed.chunk.js"
+  },
+  {
+    "revision": "532b8bdc1da33feb4755",
+    "url": "/static/js/5.532b8bdc.chunk.js"
+  },
+  {
+    "revision": "8bf5593848c23613e13a",
+    "url": "/static/js/6.8bf55938.chunk.js"
+  },
+  {
+    "revision": "bd840f5301b4ff04ccf4",
+    "url": "/static/js/7.bd840f53.chunk.js"
+  },
+  {
+    "revision": "125c1097505faa4f44075364ed4aa53f",
+    "url": "/static/media/facebook.125c1097.svg"
+  },
+  {
+    "revision": "a72050f422e3eaa59467",
+    "url": "/static/js/8.a72050f4.chunk.js"
+  },
+  {
+    "revision": "3809adc1105553bfadca",
+    "url": "/static/js/1.3809adc1.chunk.js"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "a75a5f095bf3a1155c66d012b647ca4c",
+    "url": "/static/media/tail-spin.a75a5f09.svg"
+  },
+  {
+    "revision": "eb19776728ded65b3a50d118f59f71cc",
+    "url": "/static/media/portrait.eb197767.jpg"
+  },
+  {
+    "revision": "e62ab6580976cb4e0ce4f97a9b4e4928",
+    "url": "/static/media/github.e62ab658.svg"
+  },
+  {
+    "revision": "5a100b46aa5dcadfabc907a76cf78c87",
+    "url": "/static/media/project.5a100b46.jpg"
+  },
+  {
+    "revision": "6d7c864ee430213da218a89fb0a98938",
+    "url": "/static/media/eye.6d7c864e.svg"
+  },
+  {
+    "revision": "70fc32d493a992fd71758da83fdd7e83",
+    "url": "/static/media/twitter.70fc32d4.svg"
+  },
+  {
+    "revision": "77be88a643115a1f6c21",
+    "url": "/static/css/main.4409c073.chunk.css"
+  },
+  {
+    "revision": "a72050f422e3eaa59467",
+    "url": "/static/css/8.d042060d.chunk.css"
+  },
+  {
+    "revision": "427d607e485f5ee6e06ece288f0036cf",
+    "url": "/index.html"
+  }
+];
